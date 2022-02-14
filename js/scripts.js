5d652c37c3c51e672623050c96cf989e90406c4c
shayne-wenzel <shayne.wenzel22@gmail.com>
2022-02-14 22:28:58 +0100

    Update scripts.js
    remove previous code
    add Pokémon objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,28 @@
-alert('Hello world');
-let favoriteFood = 'macNcheese';
-document.write(favoriteFood);
+let pokemonList = [{
+  name: 'Bulbasaur',
+  height: 0.7,
+  weight: 6.9,
+  type: ['grass', 'poison']
+},
+
+{
+  name: 'Charmilion',
+  height: 1.1,
+  weight: 19,
+  type: ['fire']
+},
+
+{
+  name: 'Dratini',
+  height: 1.8,
+  weight: 3.3,
+  type: ['dragpn']
+},
+
+{
+  name: 'Rayquaza',
+  height: 7,
+  weight: 206.5,
+  type: ['dragon']
+}
+];
